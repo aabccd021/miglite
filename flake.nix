d76ea031bed3e0d1f8f123a092b591dcb64f6f93
aabccd021 <aabccd021@gmail.com>
2025-05-21 21:01:29 +0700

    Revert "chore: fix formatting in flake.nix"
    This reverts commit 1b220cb34b6b6a78ec059a9381214bbc6350582a.

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
-        programs.nixfmt.strict = true;
         programs.prettier.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
@@ -44,7 +43,9 @@
         ];
       };
 
-      devShells.default = pkgs.mkShellNoCC { buildInputs = [ pkgs.nixd ]; };
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.nixd ];
+      };
 
       test = import ./test { pkgs = pkgs; };
 
